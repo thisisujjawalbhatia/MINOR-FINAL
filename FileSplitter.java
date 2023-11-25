a6ad9d13a5a11436d693baf012d60bbe1806abe1
Ishita-02 <88130678+Ishita-02@users.noreply.github.com>
2023-11-25 19:44:29 +0530

    Modified splitter code

--- FileSplitter.java
+++ FileSplitter.java
@@ -13,27 +13,16 @@ public class FileSplitter {
         }
 
         String inputFile = "personalDetails" + "User" + userId + ".txt";
-        int totalParts = outputFolders.length + 1; // Including the original file
+        int totalParts = outputFolders.length;
 
         try (FileInputStream fileInputStream = new FileInputStream(inputFile)) {
             FileChannel fileChannel = fileInputStream.getChannel();
             long fileSize = fileChannel.size();
             long partSize = fileSize / totalParts;
 
-            // Store the original file (part 1)
-            String originalFilePath = outputFolders[0] + File.separator + "User" + userId + "part1.txt";
-            try (FileOutputStream originalFileOutputStream = new FileOutputStream(originalFilePath);
-                 FileChannel originalFileChannel = originalFileOutputStream.getChannel()) {
-
-                long position = 0;
-                long remainingBytes = partSize;
-                fileChannel.transferTo(position, remainingBytes, originalFileChannel);
-            }
-
-            // Store the remaining parts (part 2 to part totalParts)
-            for (int i = 1; i < totalParts; i++) {
-                int folderIndex = i % outputFolders.length;
-                String outputFilePath = outputFolders[folderIndex] + File.separator + "User" + userId + "part" + (i + 1) + ".txt";
+            // Store each part in a separate folder (part 1 to part totalParts)
+            for (int i = 0; i < totalParts; i++) {
+                String outputFilePath = outputFolders[i] + File.separator + "User" + userId + "part" + (i + 1) + ".txt";
                 try (FileOutputStream fileOutputStream = new FileOutputStream(outputFilePath);
                      FileChannel outputChannel = fileOutputStream.getChannel()) {
 
